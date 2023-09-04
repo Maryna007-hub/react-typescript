6657f5dec4129e374738354af764831b8f88b6a7
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-05 00:06:44 +0100

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+
+import { MonthStatistics } from './pages/MonthStatistics/MonthStatistics';
+import { Home } from "./pages/Home/Home";
+
 
 
 function App() {
   return (
     <div className="App">
-    
+   <Switch>
+    <Route path='/' exact component={Home}/>
+    <Route path='/month-statistic' exact component={MonthStatistics}/>
+   </Switch>
     </div>
   )
   }
